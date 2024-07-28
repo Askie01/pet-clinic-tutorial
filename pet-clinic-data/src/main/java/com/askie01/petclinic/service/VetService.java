4119e6d28ee58062c374da8f53b1ac6f319e1fb4
Askie01 <adam.paw01@gmail.com>
2024-07-28 21:45:31 +0200

    Create service interfaces for pet clinic. Closes #4.
    Created services, which will play the role of CRUD operation services. However, these interfaces requires further implementation in order to work.
    
    This commit closes issue #4.

---
+++ pet-clinic-data/src/main/java/com/askie01/petclinic/service/VetService.java
@@ -0,0 +1,13 @@
+package com.askie01.petclinic.service;
+
+import com.askie01.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
