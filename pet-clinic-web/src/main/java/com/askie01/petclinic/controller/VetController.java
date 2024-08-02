806d622cc97e5e77291c1996c1f9be07d01feffb
Askie01 <adam.paw01@gmail.com>
2024-08-03 00:02:23 +0200

    Update Vet & Index controllers.
    Changed request mapping from getIndex to index - as it makes more sense.

--- pet-clinic-web/src/main/java/com/askie01/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/askie01/petclinic/controller/VetController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("vet")
 public class VetController {
 
-    @GetMapping("getIndex")
+    @GetMapping("index")
     public String getIndex() {
         return "vet/index";
     }
