d300a35d7a53b6ca9773e9cedee9f5c7b214932b
Askie01 <adam.paw01@gmail.com>
2024-08-07 03:41:57 +0200

    Remove @Service from interfaces.
    Removed @Service annotation on interfaces, because they cannot be instantiated so annotating this as a Spring Service stereotype doesn't make any sense.

--- pet-clinic-data/src/main/java/com/askie01/petclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/askie01/petclinic/service/VetService.java
@@ -1,8 +1,6 @@
 package com.askie01.petclinic.service;
 
 import com.askie01.petclinic.model.Vet;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface VetService extends CrudService<Vet, Long> {
 }
