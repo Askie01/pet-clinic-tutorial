1bbed61ac68237314ece28943f8ed690c7c8cacc
Askie01 <adam.paw01@gmail.com>
2024-08-07 07:49:59 +0200

    Update 'OwnerController' * Owner 'index.html'. Closes #22.
    Updated 'OwnerController' & it's 'index.html' - so now we can display all owners on index page.
    
    This commit closes issue #22.

--- pet-clinic-web/src/main/java/com/askie01/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/askie01/petclinic/controller/OwnerController.java
@@ -1,15 +1,22 @@
 package com.askie01.petclinic.controller;
 
+import com.askie01.petclinic.service.OwnerService;
+import lombok.Data;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Data
 @Controller
 @RequestMapping("owner")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
     @GetMapping("index")
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
