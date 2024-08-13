2514d8385ab63e146582d4ac4c40a2431c46987a
Askie01 <adam.paw01@gmail.com>
2024-08-13 10:37:17 +0200

    Initialize Vet's Set of speciality field.

--- pet-clinic-data/src/main/java/com/askie01/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/askie01/petclinic/model/Vet.java
@@ -3,10 +3,11 @@ package com.askie01.petclinic.model;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Vet extends Person {
-    private Set<Speciality> specialities;
+    private final Set<Speciality> specialities = new HashSet<>();
 }
