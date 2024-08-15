9098cc144b6bb6597c11bf45673c62fbcdf7d211
Askie01 <adam.paw01@gmail.com>
2024-08-15 15:53:03 +0200

    Update 'Speciality', add specialities to vets on startup. Closes #51.
    * Added 2 constructor annotations in 'Speciality' model class.
    * Updated 'VetServiceMap' to check for vet's specialities and automatically assign ids to them.
    * This commit closes #51.

--- pet-clinic-data/src/main/java/com/askie01/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/askie01/petclinic/model/Speciality.java
@@ -1,9 +1,13 @@
 package com.askie01.petclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Speciality extends BaseEntity {
     private String description;
--- pet-clinic-data/src/main/java/com/askie01/petclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/askie01/petclinic/service/map/VetServiceMap.java
@@ -1,13 +1,22 @@
 package com.askie01.petclinic.service.map;
 
+import com.askie01.petclinic.model.Speciality;
 import com.askie01.petclinic.model.Vet;
+import com.askie01.petclinic.service.SpecialtyService;
 import com.askie01.petclinic.service.VetService;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Data
 @Service
+@EqualsAndHashCode(callSuper = true)
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -20,7 +29,12 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
-        return super.save(vet);
+        if (vetExists(vet)) {
+            persistSpecialities(vet);
+            return super.save(vet);
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -31,5 +45,40 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
     @Override
     public void deleteById(Long id) {
         super.deleteById(id);
+    }
+
+    private void persistSpecialities(Vet vet) {
+        final Set<Speciality> specialities = vet.getSpecialities();
+        if (specialtiesExist(specialities)) {
+            specialities.forEach(this::persistSpeciality);
+        }
+    }
+
+    private void persistSpeciality(Speciality speciality) {
+        if (specialityExists(speciality)) {
+            saveSpeciality(speciality);
+        }
+    }
+
+    private void saveSpeciality(Speciality speciality) {
+        if (specialityIdExists(speciality)) {
+            specialtyService.save(speciality);
+        }
+    }
+
+    private boolean vetExists(Vet vet) {
+        return vet != null;
+    }
+
+    private boolean specialtiesExist(Set<Speciality> specialities) {
+        return !specialities.isEmpty();
+    }
+
+    private boolean specialityExists(Speciality speciality) {
+        return speciality != null;
+    }
+
+    private boolean specialityIdExists(Speciality speciality) {
+        return speciality.getId() != null;
     }
 }
