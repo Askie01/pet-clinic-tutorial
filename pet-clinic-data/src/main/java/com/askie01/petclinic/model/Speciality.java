b5d12343f1ef69b4c6e555786cf43be02449ed9b
Askie01 <adam.paw01@gmail.com>
2024-08-24 09:40:03 +0200

    Convert Vets to JPA Entities. Closes #54.
    * Converted 'Vet' class to JPA Entity along with 'Speciality' & 'visit'.
    * This commit closes #54.

--- pet-clinic-data/src/main/java/com/askie01/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/askie01/petclinic/model/Speciality.java
@@ -1,11 +1,15 @@
 package com.askie01.petclinic.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@Table(name = "specialties")
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
--- pet-clinic-data/src/main/java/com/askie01/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/askie01/petclinic/model/Visit.java
@@ -1,11 +1,15 @@
 package com.askie01.petclinic.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.time.LocalDate;
 
 @Data
+@Entity
+@Table(name = "visits")
 @EqualsAndHashCode(callSuper = true)
 public class Visit extends BaseEntity {
     private LocalDate date;
